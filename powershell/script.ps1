2f7955b69a88477156f7f0f86a6f3f03696c2428
Roberth Strand <me@robstr.dev>
2023-06-15 14:45:32 +0200

    feat(ps1): set to list pods
    Signed-off-by: Roberth Strand <me@robstr.dev>

--- powershell/script.ps1
+++ powershell/script.ps1
@@ -7,19 +7,19 @@ param (
 
 $ApiServer = "https://" + $ENV:KUBERNETES_SERVICE_HOST + ":" + $ENV:KUBERNETES_SERVICE_PORT
 $ServiceAccountPath = "/var/run/secrets/kubernetes.io/serviceaccount"
-#$Namespace = "$ServiceAccountPath/namespace"
+$Namespace = "$ServiceAccountPath/namespace"
 $Token = Get-Content -Path "$ServiceAccountPath/token" | ConvertTo-SecureString -AsPlainText -Force
 $CACertificate = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2("$ServiceAccountPath/ca.crt")
 
 Write-Output "#############################"
 Write-Output "#    Starting script...     #"
 Write-Output "# Connecting to the address #"
-Write-Output "# $ApiServer #"
+Write-Output "#   $ApiServer   #"
 Write-Output "#############################"
 
 $RestParams = @{
     Method = "GET"
-    Uri = "$ApiServer/api"
+    Uri = "$ApiServer/api/v1/namespaces/$Namespace/pods"
     Certificate = $CACertificate
     Authentication = "Bearer"
     Token = $Token
