98fbc0a7c15923d36217a9ec043e364bab0571a4
Roberth Strand <me@robstr.dev>
2023-06-15 14:07:02 +0200

    fix(ps1): splash
    Signed-off-by: Roberth Strand <me@robstr.dev>

--- powershell/script.ps1
+++ powershell/script.ps1
@@ -10,12 +10,18 @@ $ServiceAccountPath = "/var/run/secrets/kubernetes.io/serviceaccount"
 #$Namespace = "$ServiceAccountPath/namespace"
 $Token = Get-Content -Path "$ServiceAccountPath/token"
 $CACertificate = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2("$ServiceAccountPath/ca.crt")
-$Header = @{
-    'Authorization' = "Bearer $Token"
-}
 
 Write-Output "#########################"
 Write-Output "Starting script..."
 Write-Output "#########################"
 
-Invoke-RestMethod -Method "GET" -Certificate $CACertificate -Headers @Header -Uri "$ApiServer/api" -TimeoutSec 30
+$RestParams = @{
+    Method = "GET"
+    Uri = "$ApiServer/api"
+    Certificate = $CACertificate
+    Authentication = "Bearer"
+    Token = $Token
+    TimeoutSec = 30
+}
+
+Invoke-RestMethod @RestParams
