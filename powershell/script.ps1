fcbff16a80d0eef55b6e054525e2aae5afdbd6d1
Roberth Strand <me@robstr.dev>
2023-06-15 13:50:51 +0200

    fix(ps1): redefined type
    Signed-off-by: Roberth Strand <me@robstr.dev>

--- powershell/script.ps1
+++ powershell/script.ps1
@@ -9,7 +9,7 @@ $ApiServer = "https://kubernetes.default.svc"
 $ServiceAccountPath = "/var/run/secrets/kubernetes.io/serviceaccount"
 #$Namespace = "$ServiceAccountPath/namespace"
 $Token = Get-Content -Path "$ServiceAccountPath/token"
-$CACertificate = Get-Content -Path "$ServiceAccountPath/ca.crt"
+[System.Security.Cryptography.X509Certificates.X509Certificate2]$CACertificate = Get-Content -Path "$ServiceAccountPath/ca.crt"
 
 $Header = @{
     'Authorization' = "Bearer $Token"
