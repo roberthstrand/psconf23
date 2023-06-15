061f45e5320fd4ae89f7fb9381f152bafc3d85b3
Roberth Strand <me@robstr.dev>
2023-06-15 15:23:02 +0200

    fix(ps1): remove all cert references
    Signed-off-by: Roberth Strand <me@robstr.dev>

--- powershell/script.ps1
+++ powershell/script.ps1
@@ -9,7 +9,7 @@ $ApiServer = "https://" + $ENV:KUBERNETES_SERVICE_HOST + ":" + $ENV:KUBERNETES_S
 $ServiceAccountPath = "/var/run/secrets/kubernetes.io/serviceaccount"
 $Namespace = "$ServiceAccountPath/namespace"
 $Token = Get-Content -Path "$ServiceAccountPath/token" | ConvertTo-SecureString -AsPlainText -Force
-$CACertificate = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2("$ServiceAccountPath/ca.crt")
+#$CACertificate = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2("$ServiceAccountPath/ca.crt")
 
 Write-Output "#############################"
 Write-Output "#    Starting script...     #"
@@ -20,7 +20,7 @@ Write-Output "#############################"
 $RestParams = @{
     Method = "GET"
     Uri = "$ApiServer/api/v1/namespaces/$Namespace/pods"
-    Certificate = $CACertificate
+    #Certificate = $CACertificate
     Header = @{
         Authorization = "Bearer $Token"
     }
