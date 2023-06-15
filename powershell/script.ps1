9b52f8c4df0ba8bf51d5a63d03176d2ebef2217f
Roberth Strand <me@robstr.dev>
2023-06-15 11:30:08 +0200

    feat: initial kubernetes api example
    Signed-off-by: Roberth Strand <me@robstr.dev>

--- powershell/script.ps1
+++ powershell/script.ps1
@@ -1,1 +1,18 @@
-Write-Host 'Hello'
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $false)]
+    [String]
+    $SecretName
+)
+
+$ApiServer = "https://kubernetes.default.svc"
+$ServiceAccountPath = "/var/run/secrets/kubernetes.io/serviceaccount"
+#$Namespace = "$ServiceAccountPath/namespace"
+$Token = "$ServiceAccountPath/token"
+$CACertificate = "$ServiceAccountPath/ca.crt"
+
+$Header = @{
+    'Authorization' = "Bearer $Token"
+}
+
+Invoke-RestMethod -Method "GET" -Certificate $CACertificate -Headers $Header -Uri "$ApiServer/api"
